ce04716f4e23fd4e3b10bbf6baa4209fca80d99f
trojankai <trojankai@gmail.com>
2016-04-21 23:02:59 -0700

    ideas on filtering

--- js/app.js
+++ js/app.js
@@ -1,193 +1,237 @@
-//model
+var art = 'images/art_icon.svg';
+var cultural = 'images/culture_icon.svg';
+var science = 'images/tech_icon.svg';
+
 var initialMuseums = [
 	{
-	title: "Los Angeles County Museum of Art",
+	name: "Los Angeles County Museum of Art",
 	location:{lat:34.06379, lng: -118.35889},
-	type:"art"
+	type:art
 	},
 	{
-	title: "Museum of Contemporary Art, Los Angeles",
-	location:{lat:34.052234,
-	lng:-118.243685},
-	type:"art"
+	name: "Museum of Contemporary Art, Los Angeles",
+	location:{lat:34.053265,
+	lng:-118.250357},
+	type: art
 	},
 	{
-	title: "The Grammy Museum",
-	location: {lat:34.044820,
-		lng:-118.265384},
-	type:"art"
+	name: "The Grammy Museum",
+	location: {lat:34.044902,
+		lng:-118.264815},
+	type: art
 	},
 	{
-	title: "Hollywood Wax Museum",
+	name: "Hollywood Wax Museum",
 	location: {lat:34.10173,
 	lng:-118.33809},
-	type:"art"
+	type: art
 	},
 	{
-	title: "Natural History Museum of Los Angeles County",
+	name: "Natural History Museum of Los Angeles County",
 	location:{lat:34.01708,
 	lng:-118.2886},
-	type:"science"
+	type: science
 	},
 	{
-	title: "California Science Center",
+	name: "California Science Center",
 	location: {lat:34.01579,
 	lng:-118.28621},
-	type:"science"
+	type: science
 	},
 	{
-	title: "La Brea Tar Pits",
+	name: "La Brea Tar Pits",
 	location: {lat:34.063796, lng:-118.355391},
-	type:"science"
+	type: science
 	},
 	{
-	title: "The Peterson Automotive Museum",
+	name: "The Peterson Automotive Museum",
 	location: {lat:34.062357, lng:-118.361123},
-	type:"science"
+	type: science
 	},
 	{
-	title: "Psychiatry an Industry of Death(aka Scientology <em>Propognada</em> Museum)",
+	name: "Psychiatry an Industry of Death(aka Scientology Museum)",
 	location:{lat:34.097768,lng:-118.333751},
-	type:"science"
+	type: science
 	},
 	{
-	title: "California African American Museum",
+	name: "California African American Museum",
 	location: {lat:34.015883,lng:-118.283430},
-	type:"cultural"
+	type: cultural
 	},
 	{
-	title: "Southwest Museum of the American Indian",
+	name: "Southwest Museum of the American Indian",
 	location:{lat:34.100476,lng:-118.206010},
-	type:"cultural"
+	type: cultural
 	},
 	{
-	title: "Japanese American National Museum",
+	name: "Japanese American National Museum",
 	location: {lat:34.049442,lng:-118.238767},
-	type:"cultural"
+	type: cultural
 	},
 	{
-	title: "Chinese American Museum",
+	name: "Chinese American Museum",
 	location: {lat:34.055629,lng:-118.239225},
-	type:"cultural"
+	type: cultural
 	},
 	{
-	title: "Los Angeles Museum of the Holocaust",
+	name: "Los Angeles Museum of the Holocaust",
 	location: {lat:34.074523,lng:-118.355702},
-	type:"cultural"
+	type: cultural
 	}
 ];
-// constructor function
-function Museum(data) {
-  this.title = ko.observable(data.title);
-  this.location = ko.observable(data.location);
-  this.type = ko.observable(data.type);
 
-}
+//String to display in info window
+var content = '';
 
+//Declare Map variable and markers array
 var map;
-var marker;
 var infowindow;
-var initMap;
-var content;
+var marker;
+
 var google;
 
 
-function ViewModel(){
-  var self = this;
-	markers = [];
-
-	//array of object instead of array of Museums
-	self.museumList = ko.observableArray(initialMuseums);
-
-
-
-	//declares current Museum
-	self.currentMuseum = ko.observable(this.museumList()[0]);
-
-	//sets query observable for search/filtering museumList
-	self.query = ko.observable('');
-
-	self.search = ko.computed(function() {
-	        return ko.utils.arrayFilter(self.museumList(), function(listResult) {
-	            //Match search with items in sortedLocations() observable array
-	            var match = listResult.title.toLowerCase().indexOf(self.query().toLowerCase()) >= 0;
-
-	            // if (match) { //If result is true, show correct marker based off users search
-	            //     listResult.marker.setVisible(true);
-							//
-	            // } else {
-	            //     listResult.marker.setVisible(false); //hide markers that do not show users search results
-	            // }
-							//
-	            // return match;
-
-	        });
-	    });
-
-
-	//initialize map
-  initMap = function() {
-      var myLatLng = {lat: 34.01708, lng: -118.2886};
-      //set map options
-      var mapOptions = {
-        center: myLatLng ,
-        zoom: 12,
-        draggable: true,
-        disableDefaultUI: true
-
-      };
-      //instantiate new map
-      map = new google.maps.Map(document.getElementById('map'), mapOptions);
-			addMarkers();
-		};
-
-      var len = self.museumList().length;
-
-			//set marker for each location in museum list
-			//places markers and sets infowindow
-	var addMarkers = function(){
-			// clearMarkers();
-			for (i = 0; i < len; i++) {
-      marker = new google.maps.Marker({
-             position: new google.maps.LatLng(self.museumList()[i].location),
-             label: self.museumList()[i].type,
-						 title: self.museumList()[i].title,
-             map: map,
-             animation: google.maps.Animation.DROP
-        });
-			//adds markers to the markers array
-				markers.push(marker);
-      //initialize infowindow for markers
-    infowindow = new google.maps.InfoWindow({
-        });
-    //opens infowindow when marker is clicked, also sets content of the inforwindow
-		google.maps.event.addListener(marker, 'click', (function(marker, i) {
-           return function() {
-               infowindow.setContent(content);
-               infowindow.open(map, marker);
-							 //marker bounces on click
-							 marker.setAnimation(google.maps.Animation.BOUNCE);
-							 //stops bounce animation after 2 seconds
-							 setTimeout(function() {
-									 marker.setAnimation(null)
-							 }, 2000);
-
-           };
-      })(marker, i));
-
-      }
-
-  };
-
-
-//initialize map and markers/infowindows
-
-
-
-//AJAX requests for content
-//wikipedia
+function Museum(value) {
+    this.name = ko.observable(value.name);
+    this.location = ko.observable(value.location);
+    this.type = ko.observable(value.type);
 
 }
-var vm = new ViewModel();
 
+
+//ViewModel
+function ViewModel() {
+	//map properties to initialize
+		initMap = function(){
+			var myLatLng = {lat:34.076472, lng: -118.287430};
+			var mapOptions = {
+				center: myLatLng ,
+				zoom: 12,
+				mapTypeId: google.maps.MapTypeId.ROADMAP,
+				draggable: true,
+				disableDefaultUI: true
+			};
+			map = new google.maps.Map(document.getElementById('map'),mapOptions);
+			
+			// addMarkers();
+		};
+
+    var self = this;
+		self.markers = [];
+
+		self.museumList = ko.observableArray(initialMuseums);
+
+
+		console.log(self.museumList());//14 objects
+
+		// self.museumList().forEach(function(museumLoc){
+		// 	marker = new google.maps.Marker({
+		// 			position: museumLoc.location,
+		// 			map: map,
+		// 			title: museumLoc.name,
+		// 			icon: museumLoc.type,
+		// 			animation: google.maps.Animation.DROP
+		// 	});
+		//
+		// 	museumLoc.marker = marker;
+		// 	// console.log(museumLocation);
+		// 	// 	console.log(marker);
+		// 	this.markers.push(marker);
+		//
+		// });
+
+
+		//set up markers for museum array
+	//  addMarkers = function() {
+			for (var i = 0; i < self.museumList().length; i++) {
+				marker = new google.maps.Marker({
+					position: self.museumList()[i].location,
+					map: map,
+					title: self.museumList()[i].name,
+					icon:self.museumList()[i].type,
+					animation: google.maps.Animation.DROP
+				});
+				// console.log(museumLocation);
+				// 	console.log(marker);
+				markers.push(marker);
+	 //
+	 //
+			}
+
+				console.log(markers);//14 markers
+				// Map infowindows to each marker in markers array
+				self.markers.forEach(function(marker){
+				//create infowindow for each marker in the marker array
+					infowindow = new google.maps.InfoWindow({
+						content:this.title
+					});
+					//on click marker will bounce, set marker to center and zoom in to location
+					marker.addListener('click',function(){
+						map.panTo(this.position);
+						map.setZoom(16);
+						infowindow.open(map, this);
+						// infowindow.setContent(marker.title);
+						marker.setAnimation(google.maps.Animation.BOUNCE);
+
+						setTimeout(function(){
+							marker.setAnimation(null);
+						},2000);//stops bouncing after 2 seconds
+					});
+				});
+				//when map is clicked away from marker, map will reset
+					map.addListener('click', function() {
+						map.setCenter({lat:34.076472, lng: -118.287430});
+						map.setZoom(12);
+						infowindow.close();
+						self.query('');
+					});
+
+
+
+	// console.log(this.museumList());
+	//when list item is clicked, marker will DROP animate and infowindow will open
+	// this.clickList = function(){
+	// 	console.log(this.name);
+	// 	infowindow.close();
+	// 	console.log(self.markers);
+	// 	for (var i = 0; i < self.markers.length; i++) {
+	// 		if (this.name === markers[i].title){
+	//
+	// 			map.setCenter(markers[i].position);
+	// 			map.setZoom(16);
+	// 			console.log(markers[i].title);
+	// 			infowindow.open(map, markers[i]);
+	// 			infowindow.setContent(markers[i].title);
+	// 			markers[i].setAnimation(google.maps.Animation.DROP);
+	// 			// self.query = (this.name);
+	// 			// console.log(self.query);
+	// 		}
+	//
+	// 	}
+	// };
+	//query--search/filter variable to use for data bind
+	self.query = ko.observable('');
+	self.search = ko.computed(function() {
+			return ko.utils.arrayFilter(self.museumList(), function(museum) {
+					//Match search with items in museumList observable array
+					var match = museum.name.toLowerCase().indexOf(self.query().toLowerCase()) >= 0;//returns boolean value
+					// console.log(museum.name);
+					if(match){
+							museum.marker.setVisible(true);
+					} else {
+						museum.marker.setVisible(false);
+					}
+
+					// console.log(typeof match);
+
+					return match;
+
+
+			});
+
+	});
+
+
+}//end of ViewModel
+var vm = new ViewModel();
 ko.applyBindings(vm);
